2cc40d1dac6f1b04ed05787e685c8ccb252971ad
richie <rickromero99@gmail.com>
2019-10-12 19:49:53 -0700

    edited

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require ('express');
+const app = express ();
+const path = require ('path');
+
+
+
+app.use('/static', express.static(path.join(__dirname, "public")))
+
+
+app.get('/', function (req, res){
+    res.sendFile(path.join(__dirname, "index.html"));
+
+});
+
+app.listen(process.env.PORT || 4000, function(){
+    console.log ('your node js server is running');
+
+}); 
