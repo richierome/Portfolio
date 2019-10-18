10b78669bd99cb7904413f2c5ab05d1966df5273
richie <rickromero99@gmail.com>
2019-10-18 12:02:53 -0700

    first push to heroku

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-// const express = require ('express');
-// const app = express ();
-// const path = require ('path');
+const express = require ('express');
+const app = express ();
+const path = require ('path');
 
 
 
-// app.use('/static', express.static(path.join(__dirname, "public")))
+app.use('/static', express.static(path.join(__dirname, "public")))
 
 
-// app.get('/', function (req, res){
-//     res.sendFile(path.join(__dirname, "index.html"));
+app.get('/', function (req, res){
+    res.sendFile(path.join(__dirname, "index.html"));
 
-// });
+});
 
-// app.listen(process.env.PORT || 4000, function(){
-//     console.log ('your node js server is running');
+app.listen(process.env.PORT || 4000, function(){
+    console.log ('your node js server is running');
 
-// }); 
+}); 
