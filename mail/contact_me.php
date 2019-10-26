cf45bc78077320a096afdfb09b265d011c492180
richie <rickromero99@gmail.com>
2019-10-26 15:41:04 -0700

    mail

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -9,7 +9,7 @@ $name = strip_tags(htmlspecialchars($_POST['name']));
 $email = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
-
+var_dump($name); die();
 // Create the email and send the message
 $to = "rickromero1984@yahoo.com"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
 $subject = "Website Contact Form:  $name";
