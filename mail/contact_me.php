f12ad306fa42d6e4b5b1955353a4c0f2ab5c7c38
richie <rickromero99@gmail.com>
2019-10-26 16:05:48 -0700

    mail

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -11,12 +11,12 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 var_dump($name); die();
 // Create the email and send the message
-$to = "rickromero1984@yahoo.com"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = "https://formspree.io/xbjrkgbw"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
 $subject = "Website Contact Form:  $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
 $header = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $header .= "Reply-To: $email";	
-rickromero1984@yahoo.com
+
 if(!mail($to, $subject, $body, $header))
   http_response_code(500);
 ?>
