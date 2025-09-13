d14eaff4cf7af3234a6ffe6b742f02ffc0c09ef0
rick romero <rickromero99@gmail.com>
2025-09-13 15:20:59 -0700

    updated 2025

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const path = require ('path');
 
 
 
-app.use('/', express.static(path.join(__dirname, "public")))  // original //
+// app.use('/', express.static(path.join(__dirname, "public")))
+// use this app.use when portfolio is updated and ready to push to aws server//
 
-//  app.use("/static", express.static('./public/'));
+  app.use("/static", express.static('./public/'));
+  //use this app.use when updating portfolio to view css//
 
 
 app.get('/', function (req, res){
